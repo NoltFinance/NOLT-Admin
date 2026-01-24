42ac96af650039f60ad204853ec1760b9f575697
Dare Goodness <daregoodness@gmail.com>
2026-01-24 21:27:07 +0100

    feat: Update AuthView to use anchor tag for external link to services

--- components/AuthView.tsx
+++ components/AuthView.tsx
@@ -347,13 +347,13 @@ const AuthView: React.FC<AuthViewProps> = ({ onLoginSuccess }) => {
                         <span className="text-primary">Request Access</span>
                       </button>
                       <span className="text-slate-700 mx-2">•</span>
-                      <button
-                        type="button"
-                        onClick={() => (window.location.href = "/forms")}
+                      <a
+                        target="_blank"
+                        href={import.meta.env.VITE_CLIENT_APP || ""}
                         className="text-[10px] font-black text-slate-500 hover:text-primary uppercase tracking-widest transition-colors"
                       >
                         <span className="text-primary">Apply for Services</span>
-                      </button>
+                      </a>
                     </div>
                   </form>
                 ) : (
